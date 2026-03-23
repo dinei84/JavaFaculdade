29fac84c0139045243ccc6d72f2faae6d18ca978
dinei84 <devdinei@gmail.com>
2026-03-23 16:49:49 -0300

    Adicionando curso de spring boot do mateus

---
+++ cursoinicial/src/main/java/com/example/cursoinicial/CursoinicialApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursoinicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoinicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoinicialApplication.class, args);
+	}
+
+}
---
+++ cursoinicial/src/test/java/com/example/cursoinicial/CursoinicialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursoinicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoinicialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
